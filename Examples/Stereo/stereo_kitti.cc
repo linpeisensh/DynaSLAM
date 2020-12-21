e1246dd2dee35f3797978d7b5803035713b74af6
Peisen <linpeisensh@gmail.com>
2020-12-21 10:22:02 +0100

    dy

--- Examples/Stereo/stereo_kitti.cc
+++ Examples/Stereo/stereo_kitti.cc
@@ -107,12 +107,12 @@ int main(int argc, char **argv)
                 cv::imwrite("clmd.png",maskLeftRCNNdil);
             }
             maskLeft = maskLeft - maskLeftRCNNdil;
-            if (ni == 1){
-                cv::imwrite("clm.png",maskLeftRCNN);
-            }
             cv::Mat maskRightRCNNdil = maskRightRCNN.clone();
             cv::dilate(maskRightRCNN, maskRightRCNNdil, kernel);
             maskRight = maskRight - maskRightRCNNdil;
+            if (ni == 1){
+                cv::imwrite("clm.png",maskLeftRCNN);
+            }
         }
 
         // Pass the images to the SLAM system
